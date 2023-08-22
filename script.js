1a797643253fce1efae91367ccfe14c66c52e03f
PipZip <37911683+PipZip@users.noreply.github.com>
2023-08-22 21:54:16 +0600

    add js send form

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-var js_btn_submit = document.querySelector('.js-btn-submit');
-var thankyou_popup = document.querySelector('.thankyou-popup');
-var thankyou_popup_back = document.querySelector('.thankyou-popup__back');
-var thankyoupopup_close = document.querySelector('.thankyou-popup__close');
+const thankyou_popup = document.querySelector('.thankyou-popup');
+const thankyou_popup_back = document.querySelector('.thankyou-popup__back');
+const thankyoupopup_close = document.querySelector('.thankyou-popup__close');
+const thankyoupopup_title = document.querySelector('.thankyou-popup__title');
+const saveDataForm = document.getElementById('saveDataForm');
 
-if (js_btn_submit) {
-	js_btn_submit.addEventListener("click", function(e) {
-		thankyou_popup.classList.add('js-active')
-	});
-	thankyou_popup_back.addEventListener("click", function(e) {
-		thankyou_popup.classList.remove('js-active')
-	});
-	thankyoupopup_close.addEventListener("click", function(e) {
-		thankyou_popup.classList.remove('js-active')
-	});
-}
+document.addEventListener('DOMContentLoaded', function() {
+    if (saveDataForm) {
+        saveDataForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            const formData = new FormData(saveDataForm);
+            const xhr = new XMLHttpRequest();
+            xhr.open('POST', '/save_data/', true);
+            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    thankyoupopup_title.innerHTML = 'Спасибо за вашу заявку!';
+                    thankyou_popup.classList.add('js-active');
+                } else {
+                    thankyoupopup_title.innerHTML = 'Ошибка.<br>Что-то пошло не так. Попробуйте позже.';
+                    thankyou_popup.classList.add('js-active');
+                    console.error('Ошибка: ' + xhr.status);
+                }
+            };
+            xhr.onerror = function() {
+                thankyoupopup_title.innerHTML = 'Ошибка.<br>Что-то пошло не так. Попробуйте позже.';
+                thankyou_popup.classList.add('js-active');
+                console.error('Произошла ошибка при отправке запроса.');
+            };
+            xhr.send(formData);
+        });
+
+        thankyou_popup_back.addEventListener("click", function(e) {
+            thankyou_popup.classList.remove('js-active');
+        });
+        thankyoupopup_close.addEventListener("click", function(e) {
+            thankyou_popup.classList.remove('js-active');
+        });
+    }
+});
